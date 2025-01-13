cea23be6fa3d0644d2b3eb2fec9323c86492f9bc
Christos Alexiou <christos@budibase.com>
2025-01-13 14:10:17 +0200

    Get up to speed with capacity provider changes

--- ecs.tf
+++ ecs.tf
@@ -1,11 +1,22 @@
 resource "aws_ecs_cluster" "this" {
-  name               = var.id
-  capacity_providers = ["FARGATE"]
-  tags               = var.tags
+  name = var.id
+  tags = var.tags
 
   setting {
     name  = "containerInsights"
     value = "enabled"
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "this" {
+  cluster_name = aws_ecs_cluster.this.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
   }
 }
 
