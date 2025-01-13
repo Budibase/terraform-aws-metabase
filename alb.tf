8e29e5b15ecfe34c5bf20a61d80b2888473e3cba
Christos Alexiou <christos@budibase.com>
2025-01-13 15:30:09 +0200

    Configure ACLs and ownership controls separately

--- alb.tf
+++ alb.tf
@@ -69,13 +69,27 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 
 resource "aws_s3_bucket_lifecycle_configuration" "this" {
   bucket = aws_s3_bucket.this.bucket
-  lifecycle_rule {
-    enabled = true
+  rule {
+    status = "Enabled"
 
     expiration {
       days = var.log_retention
     }
   }
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
 }
 
 resource "aws_s3_bucket" "this" {
