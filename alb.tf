af8a0a74246076b045d372a91a042484bec09df5
Christos Alexiou <christos@budibase.com>
2025-06-12 14:15:15 +0300

    Drop the buckets

--- alb.tf
+++ alb.tf
@@ -58,81 +58,6 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  count = var.create_alb ? 1 : 0
-
-  bucket = aws_s3_bucket.this.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "this" {
-  count = var.create_alb ? 1 : 0
-
-  bucket = aws_s3_bucket.this.bucket
-  rule {
-    id = "log"
-
-    status = "Enabled"
-
-    expiration {
-      days = var.log_retention
-    }
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "this" {
-  bucket = aws_s3_bucket.this.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "this" {
-  count = var.create_alb ? 1 : 0
-
-  depends_on = [aws_s3_bucket_ownership_controls.this]
-
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "this" {
-  count = var.create_alb ? 1 : 0
-
-  bucket_prefix = "mb-"
-  force_destroy = !var.protection
-  tags          = var.tags
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_policy" "this" {
-  count = var.create_alb ? 1 : 0
-
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.s3.json
-}
-
-data "aws_elb_service_account" "this" {}
-
-data "aws_iam_policy_document" "s3" {
-  statement {
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.this.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_elb_service_account.this.arn]
-    }
-  }
-}
-
 resource "aws_security_group" "alb" {
   name_prefix = "${var.id}-alb-"
   vpc_id      = var.vpc_id
@@ -149,7 +74,7 @@ resource "aws_security_group_rule" "alb_egress_ecs" {
   from_port                = 0
   to_port                  = 0
   protocol                 = "-1"
-  security_group_id        = aws_security_group.alb.id
+  security_group_id        = aws_security_group.alb[0].id
   source_security_group_id = aws_security_group.ecs.id
 }
 
@@ -159,7 +84,7 @@ resource "aws_security_group_rule" "alb_ingress_http" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.alb[0].id
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
@@ -169,6 +94,6 @@ resource "aws_security_group_rule" "alb_ingress_https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.alb[0].id
   cidr_blocks       = ["0.0.0.0/0"]
 }
