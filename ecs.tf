48eab52568c3dfeccb7ebca2bb50ae867769c77f
Christos Alexiou <christos@budibase.com>
2025-06-12 14:32:03 +0300

    Refactor

--- ecs.tf
+++ ecs.tf
@@ -199,34 +199,3 @@ resource "aws_cloudwatch_log_group" "this" {
   retention_in_days = var.log_retention
   tags              = var.tags
 }
-
-resource "aws_security_group" "ecs" {
-  name_prefix = "${var.id}-ecs-"
-  vpc_id      = var.vpc_id
-  tags        = var.tags
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group_rule" "ecs_egress_internet" {
-  count             = var.internet_egress ? 1 : 0
-  description       = "Internet"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.ecs.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "ecs_ingress_alb" {
-  description              = "ALB"
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.ecs.id
-  source_security_group_id = aws_security_group.alb.id
-}
---
+++ sg.tf
@@ -0,0 +1,70 @@
+resource "aws_security_group" "alb" {
+  name_prefix = "${var.id}-alb-"
+  vpc_id      = var.vpc_id
+  tags        = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "alb_egress_ecs" {
+  description              = "ECS"
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.alb.id
+  source_security_group_id = aws_security_group.ecs.id
+}
+
+resource "aws_security_group_rule" "alb_ingress_http" {
+  description       = "Internet"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.alb.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "alb_ingress_https" {
+  description       = "Internet"
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.alb.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "ecs" {
+  name_prefix = "${var.id}-ecs-"
+  vpc_id      = var.vpc_id
+  tags        = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "ecs_egress_internet" {
+  count             = var.internet_egress ? 1 : 0
+  description       = "Internet"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.ecs.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "ecs_ingress_alb" {
+  description              = "ALB"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.ecs.id
+  source_security_group_id = aws_security_group.alb.id
+}
